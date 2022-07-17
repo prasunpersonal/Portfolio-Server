d19287b0537a9192257a1782a4e44093365235d2
Prasun Sarkar <prasunsarkar.personal@gmail.com>
2022-07-17 20:01:11 +0530

    Education Model Updated

--- Models/educationModel.js
+++ Models/educationModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose.model('Education', new mongoose.Schema ({
-    startingYear: {type: String, required: true},
-    endingYear: {type: String},
     title: {type: String, required: true},
+    organisation: {type: String, required: true},
+    completionYear: {type: String},
     description: {type: String}
 }));
