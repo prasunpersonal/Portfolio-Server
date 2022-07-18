bd491116a39aca4218328435953d3a8cfbbb9ce8
Prasun Sarkar <prasunsarkar.personal@gmail.com>
2022-07-18 21:10:50 +0530

    update sorting by date

--- Routers/certificateRouter.js
+++ Routers/certificateRouter.js
@@ -6,7 +6,7 @@ const fileUploadHelper = require('../Helpers/fileUpload');
 
 router.get('/all', async(req, res) => {
     try {
-        res.json((await Certificate.find()).sort((a, b) => b.certificateDate.localeCompare(a.certificateDate)));
+        res.json((await Certificate.find()).sort((a, b) => new Date(b.certificateDate).getTime() - new Date(a.certificateDate).getTime()));
     } catch (error) {
         res.json({"Error":error});
     }
--- Routers/projectRouter.js
+++ Routers/projectRouter.js
@@ -6,7 +6,7 @@ const fileUploadHelper = require('../Helpers/fileUpload');
 
 router.get('/all', async (req, res) => {
     try {
-        res.json((await Project.find()).sort((a, b) => (b.projectDate.localeCompare(a.projectDate))));
+        res.json((await Project.find()).sort((a, b) => new Date(b.certificateDate).getTime() - new Date(a.certificateDate).getTime()));
     } catch (error) {
         res.json({ "Error": error });
     }
