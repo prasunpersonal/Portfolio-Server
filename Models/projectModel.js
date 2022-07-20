6d77ebe221257bd9480d2535a045e950bd6cf6dc
Prasun Sarkar <prasunsarkar.personal@gmail.com>
2022-07-20 21:15:09 +0530

    update project model

--- Models/projectModel.js
+++ Models/projectModel.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose.model('Project', new mongoose.Schema ({
     projectTitle: {type: String, required: true},
-    projectDescription: {type: String, required: true},
     projectCategory: {type: String, required: true},
     projectDate: {type: String, required: true},
     githubLink: {type: String, required: true},
