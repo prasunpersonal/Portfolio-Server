7719d0cfeed526d97d3e6a8a33753fef490e9007
Prasun Sarkar <prasunsarkar.personal@gmail.com>
2022-07-21 22:39:22 +0530

    removed unused files

--- Helpers/fileUpload.js
+++ Helpers/fileUpload.js
@@ -1,6 +1,4 @@
 const cloudinary = require('cloudinary').v2;
-const path = require('path');
-const DatauriParser = require('datauri/parser');
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
