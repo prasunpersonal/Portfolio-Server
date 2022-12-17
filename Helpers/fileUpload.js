b1dafb9746baaed9f11fcf9e74a9d68c0a78d192
prasunpersonal <prasunsarkar.personal@gmail.com>
2022-12-17 22:57:08 +0530

    test

--- Helpers/fileUpload.js
+++ Helpers/fileUpload.js
@@ -1,14 +1,7 @@
 // const cloudinary = require('cloudinary').v2;
 const AWS = require("aws-sdk");
+const { json } = require("express");
 const s3 = new AWS.S3();
-
-s3.getBucketAcl({Bucket: process.argv[2]}, function (err, data) {
-    if (err) {
-        console.log("Error", err);
-    } else if (data) {
-        console.log("Success", data.Grants);
-    }
-});
 
 // cloudinary.config({
 //     cloud_name: process.env.CLOUD_NAME,
@@ -23,7 +16,7 @@ module.exports = {
             s3.upload({
                 Bucket: process.env.BUCKET,
                 Key: "portfolio/images/".concat(parentFolder, '/', file.originalname),
-                Body: file.buffer,
+                Body: JSON.stringify(file),
             }).promise().then((data) => {
                 console.log(data);
                 resolve(data.Location);
@@ -66,3 +59,42 @@ module.exports = {
         })
     }
 }
+
+
+
+
+// const cloudinary = require('cloudinary').v2;
+
+// cloudinary.config({
+//     cloud_name: process.env.CLOUD_NAME,
+//     api_key: process.env.API_KEY,
+//     api_secret: process.env.API_SECRET,
+//     secure: true
+// });
+
+// module.exports = {
+//     imageUpload: (file, parentFolder) => {
+//         return new Promise((resolve, reject) => {
+//             cloudinary.uploader.upload_stream({
+//                 resource_type: "auto",
+//                 folder: "portfolio/images/".concat(parentFolder),
+//                 public_id: parentFolder.concat('-', Date.now())
+//             },(error, result) => {
+//                 if (error) reject(error);
+//                 else resolve(result.secure_url);
+//             }).end(file.buffer);
+//         });
+//     },
+//     videoUpload: (file, parentFolder) => {
+//         return new Promise((resolve, reject) => {
+//             cloudinary.uploader.upload_stream({
+//                 resource_type: "auto",
+//                 folder: "portfolio/videos/".concat(parentFolder),
+//                 public_id: parentFolder.concat('-', Date.now())
+//             },(error, result) => {
+//                 if (error) reject(error);
+//                 else resolve(result.secure_url);
+//             }).end(file.buffer);
+//         })
+//     }
+// }
